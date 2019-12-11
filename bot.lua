1057c0912e520575c86afa9f35993f18305c92c3
Seniru Pasan Indira <senirupasan@gmail.com>
2019-12-11 13:14:57 +0530

    Use string.find instead of string.match
    Co-Authored-By: Lautenschlager <tai.romani@hotmail.com>

--- bot.lua
+++ bot.lua
@@ -5252,7 +5252,7 @@ commands["set"] = {
 									color = role.color,
 									title = "Promotion!",
 									thumbnail = { url = member.user.avatarURL },
-									description = "**" .. member.name .. "** is now " .. ((not not string.match(string.upper(role.name)), '^[AEIOU]')) and ' an ' or ' a ' ) .. `" .. string.upper(role.name) .. "`.",
+									description = "**" .. member.name .. "** is now " .. (string.find(role.name, "^[AEIOUaeiou]") and "a" or "an") .. " `" .. string.upper(role.name) .. "`.",
 									footer = { text = "Set by " .. message.member.name }
 								}
 							}
